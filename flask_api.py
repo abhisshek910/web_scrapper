16a23dc36a3c4a09a4dc603c2ae4f612022a0ff2
Abhishek Agrawal <74041211+abhisshek910@users.noreply.github.com>
2023-09-05 11:43:27 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import psycopg2
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='template')
 
 # Connect to PostgreSQL
 conn = psycopg2.connect(
